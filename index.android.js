79bcaf5d5faaf2f938be6801e97fb3fcfa75cc3d
wnamen <williamjosephnamen@gmail.com>
2017-02-17 15:40:47 -0800

    scaffolded project and moved code base to app folder

---
+++ index.android.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
+import App from './app/App';
+
+export default class biodesignMailMobileapp extends Component {
+  render() {
+    return (
+      <App />
+    );
+  }
+}
+
+AppRegistry.registerComponent('biodesignMailMobileapp', () => biodesignMailMobileapp);
