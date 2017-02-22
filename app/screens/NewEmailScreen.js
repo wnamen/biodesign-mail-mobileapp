b51cdff3602daa3baaef9de58907f27e45f9a4cb
wnamen <williamjosephnamen@gmail.com>
2017-02-21 18:27:40 -0800

    completed address book in email

--- app/screens/NewEmailScreen.js
+++ app/screens/NewEmailScreen.js
@@ -1,7 +1,7 @@
 'use strict'
 
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, ListView, Navigator, TouchableOpacity, TextInput } from 'react-native';
+import { StyleSheet, View, Text, ListView, Navigator, TouchableOpacity, TextInput, Image } from 'react-native';
 import NewEmailNavBar from '../components/NewEmailNavBar'
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import FaIcon from 'react-native-vector-icons/FontAwesome';
@@ -9,17 +9,30 @@ import FaIcon from 'react-native-vector-icons/FontAwesome';
 class NewEmailScreen extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      showAddressBook: false,
+      icon: 'plus'
+    };
+  }
+
+  handleAddressBook = () => {
+    let icon;
+    this.state.icon === 'plus' ? icon = 'minus' : icon = 'plus';
+
+    this.setState({
+      showAddressBook: !this.state.showAddressBook,
+      icon: icon
+    })
   }
 
   render () {
     const attachment = (<FaIcon name="paperclip" size={18} color='#9ca4ab' />)
-    const plus = (<FaIcon name="plus" size={18} color='#9ca4ab' />)
 
     return (
       <View style={{flex: 1}}>
         <NewEmailNavBar navigator={this.props.navigator}/>
         <View style={styles.container}>
+
           <View style={styles.emailDetailFields}>
             <Text style={{color: '#9ca4ab', fontSize: 15}}>To:</Text>
             <TextInput
@@ -29,8 +42,35 @@ class NewEmailScreen extends Component {
               onChangeText={(to) => this.setState({to})}
               value={this.state.to}
               />
-            { plus }
+            <TouchableOpacity onPress={this.handleAddressBook}>
+              <FaIcon name={this.state.icon} size={18} color='#9ca4ab' />
+            </TouchableOpacity>
           </View>
+
+           {this.state.showAddressBook ? (<View style={styles.addressBookContainer}>
+            <TouchableOpacity style={styles.addressBookRow} onPress={this.handleAddressBook}>
+              <Image
+                style={{height: 30, width: 30}}
+                source={{uri: 'https://static1.squarespace.com/static/526839d5e4b0a6ea6c312276/526ef8b1e4b0aa6f78f3f614/5271642ce4b03e61739879b6/1383162937731/tim+in+India+profile_circle.png'}}
+              />
+            <View style={styles.addressTextContainer}>
+                <Text style={styles.addressName}>Matt Vickers</Text>
+                <Text style={styles.addressEmail}>matth@invisionapp.com</Text>
+              </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.addressBookRow} onPress={this.handleAddressBook}>
+              <Image
+                style={{height: 30, width: 30, borderRadius: 10}}
+                source={{uri: 'https://static1.squarespace.com/static/555bc10de4b0cb6278f41a41/t/555bc301e4b0c04fc0d163c4/1432077057333/Manatee-website-01.png?format=500w'}}
+              />
+              <View style={styles.addressTextContainer}>
+                <Text style={styles.addressName}>Manatee in a Sweater</Text>
+                <Text style={styles.addressEmail}>manatee@invisionapp.com</Text>
+              </View>
+            </TouchableOpacity>
+          </View>) : (<View></View>)}
+
           <View style={styles.emailDetailFields}>
             <Text style={{color: '#9ca4ab', fontSize: 15}}>Cc/Bcc:</Text>
             <TextInput
@@ -41,6 +81,7 @@ class NewEmailScreen extends Component {
               value={this.state.ccAndBcc}
               />
           </View>
+
           <View style={styles.emailDetailFields}>
             <Text style={{color: '#9ca4ab', fontSize: 15}}>Subject:</Text>
             <TextInput
@@ -80,7 +121,6 @@ var styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    height: 50,
     paddingHorizontal: 15,
     paddingVertical: 10,
     borderBottomWidth: 1,
@@ -94,6 +134,32 @@ var styles = StyleSheet.create({
     paddingHorizontal: 15,
     paddingVertical: 10,
 
+  },
+  addressBookContainer: {
+    backgroundColor: '#fafafa',
+    flexDirection: 'column',
+  },
+  addressBookRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: 'rgba(0, 0, 0, 0.1)',
+  },
+  addressTextContainer: {
+    paddingLeft: 20
+  },
+  addressName: {
+    color: '#525253',
+    fontSize: 12,
+    fontWeight: 'bold',
+    lineHeight: 20
+  },
+  addressEmail: {
+    fontSize: 12,
+    lineHeight: 20,
+    color: '#525253'
   }
 });
 
